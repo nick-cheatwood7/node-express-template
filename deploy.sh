64c920381d3b1e0692d075cefe62d5e553d15964
nick-cheatwood7 <nick.cheatwood@gmail.com>
2022-04-20 19:58:34 -0500

    updated deploy shell

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ read USER
 docker build -t $USER/node-express-template:$VERSION .
 docker push $USER/node-express-template:$VERSION
 
-# Deploy on server (replace `root@127.0.0.1` with VPS)
+# Deploy on server (replace `root@127.0.0.1` with VPS config)
 ssh root@127.0.0.1 "docker pull $USER/node-express-template:$VERSION && docker tag $USER/node-express-template:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
